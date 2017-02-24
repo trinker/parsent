edad47d3eed292adf683051ce06f02d4652fd079
Tyler <tyler.rinker@gmail.com>
2017-02-24 10:55:05 -0500

    fixed message in annotators

--- R/annotators.R
+++ R/annotators.R
@@ -49,14 +49,14 @@ check_models_package <- function(){
     outcome <- "openNLPmodels.en" %in% list.files(.libPaths())
     if (!outcome) {
         message(paste0("Well it appears `openNLPmodels.en` is not installed.\n",
-            "This package is necessary in order to use the `entity` package.\n\nWould you like me to try and fetch it?"))
+            "This package is necessary in order to use the `parsent` package.\n\nWould you like me to try and fetch it?"))
         ans <- utils::menu(c("Yes", "No"))
         if (ans == "2") {
-            stop("Named entity extraction aborted.  Please install `openNLPmodels.en`")
+            stop("Sentence parsing aborted.  Please install `openNLPmodels.en`")
         } else {
             message("Attempting to install `openNLPmodels.en`.")
             utils::install.packages(
-                "http://datacube.wu.ac.at/src/contrib/openNLPmodels.en_1.5-1.tar.gz",
+                "http://datacube.wu.ac.at/src/contrib/openNLPmodels.en_1.5-2.tar.gz",
                 repos=NULL,
                 type="source"
             )
